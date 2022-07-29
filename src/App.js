e511adbefffde10a708dd45edd9a89b291e3800c
Anoopkumar-111 <anoopkumarshukla111@gmail.com>
2022-07-29 12:55:00 +0530

    added Navbar with ItemList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Layout/Header";
 import Products from "./components/Products/Products";
 
 
 const App=()=>{
   return (
     <div >
+      <Header/>
       <Products/>
     </div>
   );
